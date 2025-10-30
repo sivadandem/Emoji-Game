c17abbb5723362e01d3d86da8c2cabc78fe38a2f
sivadandem <sivadandem7@gmail.com>
2025-10-30 11:50:13 +0000

    nothing2

--- src/components/EmojiCard/index.js
+++ src/components/EmojiCard/index.js
@@ -1,22 +1,20 @@
-// Write your code here.
-
 import './index.css'
-import {Component} from 'react'
 
-class EmojiCard extends Component {
-  render() {
-    const {emoji, onClickEmoji} = this.props
-    const {emojiName, emojiUrl, id} = emoji
-    /* onClickEmojiItem sends clicked emoji id to EmojiGame main page  */
-    const onclickEmojiItem = () => {
-      onClickEmoji(id)
-    }
-    return (
-      <li className="emoji-item" onClick={onclickEmojiItem}>
-        <img key={id} src={emojiUrl} alt={emojiName} className="emoji-image" />
-      </li>
-    )
+const EmojiCard = props => {
+  const {emojiDetails, clickEmoji} = props
+  const {id, emojiName, emojiUrl} = emojiDetails
+
+  const onClickEmoji = () => {
+    clickEmoji(id)
   }
+
+  return (
+    <li className="emoji-item">
+      <button className="emoji-button" type="button" onClick={onClickEmoji}>
+        <img src={emojiUrl} alt={emojiName} className="emoji-img" />
+      </button>
+    </li>
+  )
 }
 
 export default EmojiCard
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,43 +1,28 @@
-// Write your code here.
-
 import './index.css'
-import {Component} from 'react'
 
-class NavBar extends Component {
-  /* gets top score and current score from EmojiGame page */
-  /* isGameEnd is used decide whether display navbar score card or not  */
-  renderScores = () => {
-    const {currentScore, topScore, isGameEnd} = this.props
-    if (isGameEnd) {
-      return null
-    }
-    return (
-      <div className="Score-container">
-        <p className="score-label">
-          Score : <span className="score-value">{currentScore}</span>
-        </p>
-        <p className="score-label">
-          Total Score : <span className="score-value">{topScore}</span>
-        </p>
-      </div>
-    )
-  }
+const NavBar = props => {
+  const {currentScore, topScore, isGameInProgress} = props
 
-  render() {
-    return (
-      <div className="navbar-container">
+  return (
+    <nav className="navbar-container">
+      <div className="navbar-sub-container">
         <div className="logo-title-container">
           <img
-            className="logo-image"
+            className="emoji-logo"
             src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
-            alt="logo"
+            alt="emoji logo"
           />
-          <p className="logo-title">Emoji Game</p>
+          <h1 className="title">Emoji Game</h1>
         </div>
-        {this.renderScores()}
+        {isGameInProgress && (
+          <div className="scores-container">
+            <p className="score">Score: {currentScore}</p>
+            <p className="score">Top Score: {topScore}</p>
+          </div>
+        )}
       </div>
-    )
-  }
+    </nav>
+  )
 }
 
 export default NavBar
